4db377ef30017a639dd08c8db675be511c90c7bb
Gilbert <taya87136@.gmail.com>
2021-04-13 23:25:24 +0800

    save data in scene delegate

--- AllListsViewController.swift
+++ AllListsViewController.swift
@@ -146,10 +146,7 @@ extension AllListsViewController: ListDetailViewDelegate {
     }
     
     func listDetailView(_ listDetailView: ListDetailViewController, didFinishEditing checkList: CheckList) {
-        checkLists.updateList()
-        let index = checkLists.getListIndex(for: checkList)
-        
-        if let index = index, let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0)) {
+        if let index = checkLists.getListIndex(for: checkList), let cell = tableView.cellForRow(at: IndexPath(row: index, section: 0)) {
             cell.textLabel?.text = checkList.name
         }
         
--- CheckLists/CheckLists.swift
+++ CheckLists/CheckLists.swift
@@ -15,39 +15,33 @@ class CheckLists {
     private var lists = [CheckList]()
     
     init() {
-        let list1 = CheckList(name: "Today")
-        list1.addNewItem(Item(label: "sex"))
-        list1.addNewItem(Item(label: "fitness factory"))
-        list1.addNewItem(Item(label: "leetcode"))
+//        let list1 = CheckList(name: "Today")
+//        list1.addNewItem(Item(label: "sex"))
+//        list1.addNewItem(Item(label: "fitness factory"))
+//        list1.addNewItem(Item(label: "leetcode"))
+//
+//        let list2 = CheckList(name: "Tomorrow")
+//        list2.addNewItem(Item(label: "Swim"))
+//        list2.addNewItem(Item(label: "ios learn"))
+//        list2.addNewItem(Item(label: "support customer"))
+//
+//        lists = [
+//        list1, list2,
+//        ]
         
-        let list2 = CheckList(name: "Tomorrow")
-        list2.addNewItem(Item(label: "Swim"))
-        list2.addNewItem(Item(label: "ios learn"))
-        list2.addNewItem(Item(label: "support customer"))
-        
-        lists = [
-        list1, list2,
-        ]
-        
-//        loadCheckLists()
+        loadCheckLists()
     }
     
     public func createNewList(name: String) {
         lists.append(CheckList(name: name))
-        saveCheckLists()
     }
     
     public func removeList(by index: Int) {
         if (0..<lists.count).contains(index) {
             lists.remove(at: index)
         }
-        saveCheckLists()
     }
-    
-    public func updateList() {
-        saveCheckLists()
-    }
-    
+        
     public func getListIndex(for list: CheckList) -> Int? {
         return lists.firstIndex(of: list)
     }
@@ -71,6 +65,7 @@ extension CheckLists {
         do {
             let data = try encoder.encode(lists)
             try data.write(to: dataPath(), options: .atomic)
+            print("Save Current Data to \(dataPath())")
         } catch  {
             print("Error encoding item array: \(error.localizedDescription)")
         }
@@ -83,6 +78,7 @@ extension CheckLists {
             
             do {
                 lists = try decoder.decode([CheckList].self, from: data)
+                print("Load Current Data from \(dataPath())")
             } catch {
                 print("Error decoding list array: \(error.localizedDescription)")
             }
